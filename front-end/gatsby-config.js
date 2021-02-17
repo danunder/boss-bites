e632e2a5596cc5028e55f51ad776d627e91edcaa
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-17 13:45:36 -0500

    connect gatsby framework to shopify store

--- front-end/gatsby-config.js
+++ front-end/gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
     plugins: [
+        {
+    resolve: `gatsby-source-shopify`,
+    options: {
+      // The domain name of your Shopify shop.
+      shopName: `BOSSBITES`,
+      // The storefront access token
+      accessToken: `aa4b9696ed83deebf840668e65d23913`,
+    },
+  },
 
     ]
 }
