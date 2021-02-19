62e523696a3d1eff956b1bcbc936eec4d33447fb
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-19 16:07:31 -0500

    continue adding theme

---
+++ front-end/plugins/custom-mui-theme/theme/index.js
@@ -0,0 +1,13 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#e6a3a3',
+    },
+    secondary: {
+      main: '#993f3f',
+    },
+  },
+};
